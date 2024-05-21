2a68e915680209b01a26292c1b41ccd72f8c1df4
Gary Zhou <117497221+GaryZhous@users.noreply.github.com>
2024-05-21 12:32:46 -0400

    Update Serveo.sh

--- Serveo.sh
+++ Serveo.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
-#powered by serveo.net
+# Powered by serveo.net
 
+# Check if the local port number is provided as an argument
 if [ $# -eq 0 ]; then
     echo "Usage: ./Serveo.sh <local_port> [remote_port]"
-    echo "If remote_port is not provided, a default port will be used by Serveo."
+    echo "If remote_port is not provided, port 80 will be used by default."
     exit 1
 fi
 
 local_port=$1
-remote_port=$2
-
-if [ -z "$2" ]; then
-    echo "No remote port provided, using only local port: $local_port"
-    remote_port=80
-fi
+remote_port=${2:-80}
 
 echo "Starting Serveo tunnel from public URL to localhost:$local_port with remote port $remote_port"
 ssh -R $remote_port:localhost:$local_port serveo.net
